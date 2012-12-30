4f76b58dcf2df8c248f04e439729bc08bcfbc971
fschwiet <fschwiet@gmail.com>
2012-12-30 14:57:08 -0800

    first commit (command list device drivers)

---
+++ Faudio/ListDevicesCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.DirectSound;
+using ManyConsole;
+
+namespace Faudio
+{
+    public class ListDevicesCommand : ConsoleCommand
+    {
+        public ListDevicesCommand()
+        {
+            this.IsCommand("list-devices");
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            Console.WriteLine("Inputs:");
+
+            foreach (var device in new AudioDeviceCollection(AudioDeviceCategory.Capture))
+            {
+                Console.WriteLine("    {0} - {1}", device.Guid, device.Description);
+            }
+
+            Console.WriteLine("Outputs:");
+            foreach (var device in new AudioDeviceCollection(AudioDeviceCategory.Output))
+            {
+                Console.WriteLine("    {0} - {1}", device.Guid, device.Description);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Faudio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faudio
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var programs = ManyConsole.ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof (Program));
+
+            try
+            {
+                return ManyConsole.ConsoleCommandDispatcher.DispatchCommand(programs, args, Console.Out);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine(e);
+            }
+
+            return 0;
+        }
+    }
+}
